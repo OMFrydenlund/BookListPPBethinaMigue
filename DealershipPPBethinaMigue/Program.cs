78c0a3808da676c0b7fa53544233fa7b5b463924
OMFrydenlund <migfrydenlund@gmail.com>
2024-12-04 12:44:39 +0100

    Changes

--- DealershipPPBethinaMigue/Program.cs
+++ DealershipPPBethinaMigue/Program.cs
@@ -4,7 +4,9 @@ List<BookModel> bookList = new List<BookModel>();
 {
     new BookModel("Diavola", 2024, "Jennifer Marie Thorne", "Horror", 296, "" +
         "Anna has two rules for the annual Pace family destination vacations: Tread lightly and survive."),
-    new BookModel()
-    new BookModel()
-    new BookModel()
+    new BookModel("", , "", "", , ""),
+    new BookModel("", , "", "", , ""),
+    new BookModel("", , "", "", , ""),
+    new BookModel("", , "", "", , ""),
+   
 };
