b0334deb448a8e556c3ab1f7866c88cca0de800c
OMFrydenlund <migfrydenlund@gmail.com>
2024-12-04 13:34:35 +0100

    Mye logikk L O L

--- DealershipPPBethinaMigue/BookModel.cs
+++ DealershipPPBethinaMigue/BookModel.cs
@@ -8,21 +8,81 @@ namespace BookListPPBethinaMigue
 {
     internal class BookModel
     {
-        public string _name { get; set; }
+        public string _title { get; set; }
         public int _year { get; set; }
         public string _author { get; set; }
         public string _genre { get; set; }
         public int _pages { get; set; }
         public string _description { get; set; }
 
+        public BookModel()
+        {
+            
+        }
         public BookModel(string name, int year, string author, string genre, int pages, string description)
         {
-            _name = name;
+            _title = name;
             _year = year;
             _author = author;
             _genre = genre;
             _pages = pages;
             _description = description;
+        }
+
+        public void GetAllBookInfo(BookModel book)
+        {
+            book.GetTitle();
+            book.GetYear();
+            book.GetAuthor();
+            book.GetGenre();
+            book.GetPages();
+            book.GetDescription();
+        }
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine("************************");
+            Console.Write($"Title: {_title}, by {_author}\nPublished: {_year}, {_genre}\n{_pages} pages\n\n{_description}\n\n");            
+        }
+
+        void GetTitle()
+        {
+            Console.Write("What is the title of your book: ");
+            string input = Console.ReadLine();
+            _title = input;
+        }
+        void GetYear()
+        {
+            Console.Write("Published when: ");
+            int input = int.Parse( Console.ReadLine());
+            _year = input;
+        }
+        void GetAuthor()
+        {
+            Console.Write("Who is the author: ");
+            string input = Console.ReadLine();
+            _author = input;
+        }
+
+        void GetGenre()
+        {
+            Console.Write("What genre: ");
+            string input = Console.ReadLine();
+            _genre = input;
+        }
+
+        void GetPages()
+        {
+            Console.Write("How many pages: ");
+            int input = int.Parse(Console.ReadLine());
+            _pages = input;
+        }
+
+        void GetDescription()
+        {
+            Console.Write("Give a short description of your book: ");
+            string input = Console.ReadLine();
+            _description = input;
         }
     }
 }
--- DealershipPPBethinaMigue/Program.cs
+++ DealershipPPBethinaMigue/Program.cs
@@ -1,12 +1,67 @@
 ﻿using BookListPPBethinaMigue;
 
 List<BookModel> bookList = new List<BookModel>();
+
+// HARDCODED VALUES
+//{
+//    new BookModel("Diavola", 2024, "Jennifer Marie Thorne", "Horror", 296, "" +
+//        "Anna has two rules for the annual Pace family destination vacations: Tread lightly and survive."),
+//    new BookModel("Lady Macbeth", 2024, "Ava Reid", "Fantasy", 320, "The Lady knows the stories: how her eyes induce madness in men. "),
+//    new BookModel("Circe", 2018, "Madeline Miller", "Fiction", 393, 
+//        "In the house of Helios, god of the sun and mightiest of the Titans, a daughter is born. "),
+//};
+
+Console.Write("Welcome to your personal book app!");
+bool isChoosing = true;
+while (isChoosing)
 {
-    new BookModel("Diavola", 2024, "Jennifer Marie Thorne", "Horror", 296, "" +
-        "Anna has two rules for the annual Pace family destination vacations: Tread lightly and survive."),
-    new BookModel("", , "", "", , ""),
-    new BookModel("", , "", "", , ""),
-    new BookModel("", , "", "", , ""),
-    new BookModel("", , "", "", , ""),
-   
-};
+    Console.Write("Choose an action:\n1. Add book\n2. View your library\n3. Exit\n\n");
+    string userChoice = Console.ReadLine();
+    switch (userChoice)
+    {
+        case "1":
+            CreateNewBook();
+            break;
+        case "2":
+            DoWeHaveBooks(bookList);
+            break;
+        case "3":
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+}
+
+void CreateNewBook()
+{
+    BookModel newBook = new BookModel();
+    newBook.GetAllBookInfo(newBook);
+    bookList.Add(newBook);
+    Console.WriteLine($"{newBook._title} has been added to the library!");
+    Thread.Sleep(2000);
+}
+
+void DoWeHaveBooks(List<BookModel> listOfBooks)
+{
+    if (listOfBooks.Count == 0)
+    {
+        Console.WriteLine("There are no books in your library.");
+    }
+    else
+    {
+        ShowBookList(listOfBooks);
+    }
+}
+
+void ShowBookList(List<BookModel> listOfBooks)
+{
+    foreach (BookModel book in listOfBooks)
+    {
+        book.PrintBookInfo();        
+    }
+}
+
+Console.ReadLine();
+
